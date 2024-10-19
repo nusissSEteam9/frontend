4d6d4d4a1a0281ed7fd92a0270a5aba7431fb0f3
chenyu <63953834+chenyu-01@users.noreply.github.com>
2024-10-19 14:34:07 +0800

    fix: correct middleware redirect logic

--- middleware/signin.ts
+++ middleware/signin.ts
@@ -1,8 +1,15 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const token = useCookie('token');
   // signup page can be accessed by anyone
-  if (!!token) {
-    alert('You are already logged in, click to redirect to home page');
-    return navigateTo('/');
+  if (to.path === '/user/login' || to.path === '/user/register') {
+    if (token.value) {
+      alert('You are already logged in');
+      return navigateTo('/');
+    }
+  }
+  // Redirect to login if not authenticated, home page can be accessed by anyone
+  else if (!token.value && to.path !== '/') {
+    alert('You need to login to access this page');
+    return navigateTo('/user/login');
   }
 });
