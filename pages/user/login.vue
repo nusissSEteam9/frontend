732fc06a698e2bd7b928bb34e7a7f20cd8e53bc6
chenyu <63953834+chenyu-01@users.noreply.github.com>
2024-10-19 16:15:32 +0800

    chore: use real backend proxy url from env

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -74,11 +74,9 @@ const handleSubmit = async () => {
     errorMessage.value = error.response.data.message;
   }
 };
-const config = useRuntimeConfig();
-console.log(config.public.backendProxyUrl);
 const logout = () => {
   $fetch('/api/auth/logout', {
-    baseURL: config.public.backendProxyUrl,
+    baseURL: process.env.NUXT_BACKEND_PROXY_URL,
     onResponse: (message) => {
       console.log(message);
     },
