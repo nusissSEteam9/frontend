6eb528c18d5cfb4186aeaa247ff16c5c802f9cb6
chenyu <63953834+chenyu-01@users.noreply.github.com>
2024-10-22 18:33:02 +0800

    fix: set correct useAuthStore

--- pages/index.vue
+++ pages/index.vue
@@ -150,7 +150,8 @@
 </template>
 
 <script setup>
-import { useAuthStore } from '@/store/auth';
+import { useAuthStore } from '~/stores/auth';
+
 const authStore = useAuthStore();
 const searchQuery = ref('');
 const searchType = ref('');
