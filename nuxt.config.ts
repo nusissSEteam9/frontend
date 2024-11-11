6104f6bdb278ee6f1c966473ca2d110a06b03dec
chenyu <63953834+chenyu-01@users.noreply.github.com>
2024-11-11 15:43:47 +0800

    install testing modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@pinia/nuxt'],
+  modules: ['@nuxt/eslint', '@pinia/nuxt', '@nuxt/test-utils/module'],
   app: {
     head: {
       charset: 'utf-8',
